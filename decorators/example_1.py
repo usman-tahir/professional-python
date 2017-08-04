8b7a010065b19d9373e6d24ad6f2dc3e9aae3caa
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-03 21:11:47 -0400

    used the preferred annotation method for function decoration

--- decorators/example_1.py
+++ decorators/example_1.py
@@ -3,9 +3,11 @@ def decorated_by(func):
     func.__doc__ += '\nDecorated by the decorated_by function'
     return func
 
+# Preferred way of decoration
+@decorated_by
 def add(x, y):
     '''Return the sum of x and y'''
     return x + y
 
-add = decorated_by(add)
+# add = decorated_by(add)
 help(add)
