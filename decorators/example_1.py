bcaae50dcecbdefd4cb7efc07fe50c1eeb9fb094
Usman Tahir <usmantahir@Usmans-iMac.local>
2017-08-03 21:07:00 -0400

    added a basic decorator example

---
+++ decorators/example_1.py
@@ -0,0 +1,11 @@
+
+def decorated_by(func):
+    func.__doc__ += '\nDecorated by the decorated_by function'
+    return func
+
+def add(x, y):
+    '''Return the sum of x and y'''
+    return x + y
+
+add = decorated_by(add)
+help(add)
