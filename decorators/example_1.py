75256a5f1f76d26a982bf03a63df2ce8880e7e89
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-03 21:22:14 -0400

    added an example with two decorators (for resolution checking purposes

--- decorators/example_1.py
+++ decorators/example_1.py
@@ -3,7 +3,12 @@ def decorated_by(func):
     func.__doc__ += '\nDecorated by the decorated_by function'
     return func
 
+def also_decorated_by(func):
+    func.__doc__ += '\nDecorated by the also_decorated_by function'
+    return func
+
 # Preferred way of decoration
+@also_decorated_by
 @decorated_by
 def add(x, y):
     '''Return the sum of x and y'''
