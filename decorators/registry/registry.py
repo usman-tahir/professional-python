b7ad0584f88102dbed8ab8e09547b5a20b565519
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-03 21:33:49 -0400

    refactored the registry method into a class, with usage examples

--- decorators/registry/registry.py
+++ decorators/registry/registry.py
@@ -1,21 +1,35 @@
 
-registry = []
+class Registry(object):
+    def __init__(self):
+        self._functions = []
 
-def register(decorated):
-    registry.append(decorated)
-    return decorated
+    def register(self, decorated):
+        self._functions.append(decorated)
+        return decorated
 
-@register
-def foo():
-    return 3
+    def run_all(self, *args, **kwargs):
+        return_values = []
+        for function in self._functions:
+            return_values.append(function(*args, **kwargs))
+        return return_values
 
-@register
-def bar():
-    return 5
+# Usage of the Registry class
+a = Registry()
+b = Registry()
 
-# Execute the functions inside the registry
-answers = []
-for function in registry:
-    answers.append(function())
+@a.register
+def foo(x = 3):
+    return x
 
-print("answers: %s" % str(answers))
+@b.register
+def bar(x = 5):
+    return x
+
+@a.register
+@b.register
+def baz(x = 7):
+    return x
+
+# Run the registry methods
+print("run all for @a: " + str(a.run_all()))
+print("run all for @b: " + str(b.run_all()))
