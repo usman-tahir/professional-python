2f1bcdbe5478ed36a295b7b29ac64e1cd6f9fdeb
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-06 08:20:15 -0400

    small refactoring for the generator function; added a usage example

--- generators/example_1.py
+++ generators/example_1.py
@@ -8,3 +8,11 @@ def fibonacci():
             numbers.append(sum(numbers))
             numbers.pop(0)
         yield numbers[-1]
+        continue
+
+# Create a generator
+g = fibonacci()
+print(g)
+
+for i in range(10):
+    print(next(g))
