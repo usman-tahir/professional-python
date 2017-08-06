9ceb99fefc410a900828fa22ccf7e272409005aa
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-06 08:40:46 -0400

    refactored the squares example

--- generators/squares.py
+++ generators/squares.py
@@ -1,11 +1,16 @@
 
-def squares():
+def squares(cursor = 1):
     cursor = 1
     while True:
-        yield cursor ** 2
-        cursor += 1
+        response = yield cursor ** 2
+        if response:
+            cursor = int(response)
+        else:
+            cursor += 1
 
 # Print out squares up to 10
 s = squares()
-for i in range(1, 11):
-    print(next(s))
+print(next(s))
+print(next(s))
+print(s.send(7))
+print(next(s))
