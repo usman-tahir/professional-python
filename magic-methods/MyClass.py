462df08b9899ee0855a545c4e2b9616ca38bb9a0
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-19 07:27:34 -0400

    implemented some example class-level functionality for the MyClass class

--- magic-methods/MyClass.py
+++ magic-methods/MyClass.py
@@ -1,14 +1,27 @@
 
 class MyClass(object):
+    def __new__(cls):
+        instance = super(MyClass, cls).__new__(cls)
+        print('The __new__ method is working')
+        return instance
+
     def __init__(self):
         print('The __init__ method is working')
     
     def __eq__(self, other):
         return type(self) == type(other)
+    
+    def __del__(self):
+        print('The __del__ method was called')
 
 # Testing out initialization
 m = MyClass()
 
 # Testing out the equals operator
-print(MyClass() == MyClass()) # True
-print(MyClass() == 42) # False
+# print(MyClass() == MyClass()) # True
+# print(MyClass() == 42) # False
+
+# Testing out deletion/garbage collection
+# When 'foo' is printed, the MyClass variable m
+# is garbage collected
+print("foo")
