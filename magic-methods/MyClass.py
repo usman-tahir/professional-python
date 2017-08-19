5678c774669d1496449ae884bb1d17735d147318
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-19 07:51:27 -0400

    refactored MyClass' equals method and associated example

--- magic-methods/MyClass.py
+++ magic-methods/MyClass.py
@@ -1,21 +1,31 @@
 
 class MyClass(object):
-    def __new__(cls):
-        instance = super(MyClass, cls).__new__(cls)
-        print('The __new__ method is working')
-        return instance
+    # def __new__(cls):
+    #     instance = super(MyClass, cls).__new__(cls)
+    #     print('The __new__ method is working')
+    #     return instance
 
-    def __init__(self):
-        print('The __init__ method is working')
+    # def __init__(self):
+    #     print('The __init__ method is working')
     
     def __eq__(self, other):
-        return type(self) == type(other)
+        print('The following are being tested for equivalence:\n%r\n%r' % (self, other))
+        return self is other
     
-    def __del__(self):
-        print('The __del__ method was called')
+    # def __del__(self):
+    #     print('The __del__ method was called')
+
+class Unequal(object):
+    def __eq__(self, other):
+        return False
+
+class MySubclass(MyClass):
+    def __eq__(self, other):
+        print('MySubclass\' __eq__ method is testing:\n%r\n%r' % (self, other))
+        return False
 
 # Testing out initialization
-m = MyClass()
+# m = MyClass()
 
 # Testing out the equals operator
 # print(MyClass() == MyClass()) # True
@@ -24,4 +34,16 @@ m = MyClass()
 # Testing out deletion/garbage collection
 # When 'foo' is printed, the MyClass variable m
 # is garbage collected
-print("foo")
+# print("foo")
+
+c1 = MyClass()
+c2 = MyClass()
+print(c1 == c2)
+print(c2 == c1)
+print(c1 == c1)
+
+print(MyClass() == Unequal())
+print(Unequal() == MyClass())
+
+print(MyClass() == MySubclass())
+print(MySubclass() == MyClass())
